a9f2ceb29713807eb6273a4bcdef3d848136674d
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-09-11 14:47:07 +0530

    d-28: some code improvement and img title fetched

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import requests
 
 API_KEY = "xUcQJT0jkvnh706V8NwiilbAAJ4LdkmITjPUDD6w"
 url = "https://api.nasa.gov/planetary/apod?" \
-      "api_key=xUcQJT0jkvnh706V8NwiilbAAJ4LdkmITjPUDD6w"
+      f"{API_KEY}"
 
 
+# Getting data for the page
 response = requests.get(url)
 content = response.json()
 img_url = content["hdurl"]
 info = content["explanation"]
+title = content["title"]
 
-st.title("This is the image")
+st.title(title)
 st.image(img_url)
 st.info(info)
 
